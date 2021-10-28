87986c0ccc0ca957e930eae6694ca3d0c57a094c
stefanofilippini <filippini.stefano96@gmail.com>
2021-10-28 15:47:25 +0200

    2nd snack 3 done

--- js-snacks-2/snack-3/main.js
+++ js-snacks-2/snack-3/main.js
@@ -1,14 +1,21 @@
 console.log('OK JS');
 
 /* 
-Il software deve chiedere per 5 volte all’utente di inserire un numero.
- Il programma stampa la somma di tutti i numeri inseriti con for
- e opzionalmente con while (edited) 
+Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una
+lista di cognomi, Gatsby vuole generare una falsa lista di 3 invitati.
 */
-let somma = 0
-for (i=0; i<5; i++) {
-    const new_Numb = parseInt(prompt('inserisci un numero'))
-    somma = somma + new_Numb
+
+const name_list = ['Marco', 'Luca', 'Mirko', 'Mattia', 'Stefano']
+const lastname_list = ['Davoli', 'Morellini', 'Marciano', 'Barbieri', 'Filippini']
+
+let name_number
+let lastname_number
+const random_names = []
+
+for (i=0; i<3; i++) {
+    name_number = Math.floor(Math.random() * 5);
+    lastname_number = Math.floor(Math.random() * 5);
+    random_names.push(`${name_list[name_number]} ${lastname_list[lastname_number]}`)
 }
 
-console.log(somma);
+console.log(random_names);
